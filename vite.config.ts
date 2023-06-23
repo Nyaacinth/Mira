673440898b52c3bda3f2b05b46c09627e763ed00
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-06-23 13:19:08 +0800

    Remove maskable

--- vite.config.ts
+++ vite.config.ts
@@ -34,19 +34,19 @@ export default defineConfig(() => {
                                   src: "pwa-icon-192x192.png",
                                   sizes: "192x192",
                                   type: "image/png",
-                                  purpose: "maskable"
+                                  purpose: "any"
                               },
                               {
                                   src: "pwa-icon-512x512.png",
                                   sizes: "512x512",
                                   type: "image/png",
-                                  purpose: "maskable"
+                                  purpose: "any"
                               },
                               {
                                   src: "pwa-icon-1024x1024.png",
                                   sizes: "1024x1024",
                                   type: "image/png",
-                                  purpose: "maskable"
+                                  purpose: "any"
                               }
                           ]
                       },
