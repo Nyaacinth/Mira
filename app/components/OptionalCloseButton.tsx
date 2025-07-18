cab8d8876a582136de4748da7798f7d19f7d1d9a
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2025-07-18 16:08:01 +0800

    Fix Close Button

--- app/components/OptionalCloseButton.tsx
+++ app/components/OptionalCloseButton.tsx
@@ -9,7 +9,7 @@ export const OptionalCloseButton: Component = () => {
     return (
         <Show when={isWin()}>
             <div
-                class="z-9999 fixed right-2 top-2 text-black w-min h-min bg-[#0000000F] rounded-[9999px]"
+                class="z-9999 fixed right-2 top-2 text-black w-min h-min bg-[#FFFFFF76] rounded-[9999px]"
                 onClick={() => getCurrentWindow().close()}
             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
